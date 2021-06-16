eddf2a2a608ac1bdec238d7a85d69f76b0308cdb
Érico Vieira Porto <ericoporto2008@gmail.com>
2021-06-16 17:24:38 -0300

    test size persian

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #define SDL_MAIN_HANDLED
 #include <SDL2/SDL.h>
 #include <SDL_ttf.h>
+#include <hb.h>
+#include <cstdio>
 
 int main()
 {
@@ -10,9 +12,18 @@ int main()
 
     SDL_Window * window = SDL_CreateWindow("SDL2_ttf Playground", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
     SDL_Surface *surface = SDL_GetWindowSurface(window);
-    TTF_Font *font = TTF_OpenFont("TwitterColorEmoji-SVGinOT-ThickFallback.ttf", 72);
+    TTF_Font *font = TTF_OpenFont("DejaVuSans.ttf", 72);
     SDL_Color color = { 255, 255, 255, 255 };
-    SDL_Surface *text = TTF_RenderUTF8_Blended(font, "😄", color);
+    TTF_SetScript(HB_SCRIPT_ARABIC);
+    TTF_SetDirection(HB_DIRECTION_RTL);
+
+    const char * textstr = "هنا مثال حيث يمكنك الكتابة من اليسار";
+    int w;
+    TTF_SizeUTF8(font,textstr,&w, nullptr);
+    printf("%d\n", w);
+
+    SDL_Surface *text = TTF_RenderUTF8_Blended(font, textstr, color);
+    printf("%d\n", text->w);
     SDL_BlitSurface(text, NULL, surface, NULL);
 
     SDL_bool running = SDL_TRUE;
